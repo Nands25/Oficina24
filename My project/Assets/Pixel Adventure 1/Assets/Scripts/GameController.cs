9380aa3232a8d5764da00487d6a79147e6626372
Nanda <fernanda.araujo1@escolar.ifrn.edu.br>
2024-08-28 14:54:58 -0300

    Vídeo 10

--- My project/Assets/Pixel Adventure 1/Assets/Scripts/GameController.cs
+++ My project/Assets/Pixel Adventure 1/Assets/Scripts/GameController.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public int totalScore;
     public Text scoreText;
     public static GameController instance;
+    public GameObject gameOver;
+    
     void Start()
     {
         instance = this;
@@ -17,5 +20,15 @@ public class GameController : MonoBehaviour
     {
         scoreText.text = totalScore.ToString();
     }
-    
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName)
+    {
+        SceneManager.LoadScene(lvlName);
+    }
+
 }
